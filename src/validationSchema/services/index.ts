77cb2d7b929d4551aa992b09bda00cce9a03a72c
roq-ai <>
2023-08-29 03:56:34 +0000

    Initial commit

---
+++ src/validationSchema/services/index.ts
@@ -0,0 +1,9 @@
+import * as yup from 'yup';
+
+export const serviceValidationSchema = yup.object().shape({
+  name: yup.string().required(),
+  description: yup.string().nullable(),
+  cost: yup.number().integer().nullable(),
+  billing: yup.string().nullable(),
+  organization_id: yup.string().nullable().required(),
+});
